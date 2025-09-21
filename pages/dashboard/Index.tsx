6f23e4344b9be2f55f711555a2051050ef3870bc
voycebutler18 <voycebutler1@gmail.com>
2025-09-21 13:16:44 -0500

    Create Index.tsx

---
+++ pages/dashboard/Index.tsx
@@ -0,0 +1,7 @@
+// finance/pages/dashboard/Index.tsx
+import React from "react";
+import FinancialDashboard from "@/components/Dashboard/FinancialDashboard";
+
+export default function DashboardIndex() {
+  return <FinancialDashboard />;
+}
