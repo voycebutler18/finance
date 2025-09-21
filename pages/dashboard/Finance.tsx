5de0d2ec049c1b3dedb1654aebc494e758fd8e02
voycebutler18 <voycebutler1@gmail.com>
2025-09-21 13:17:04 -0500

    Create Finance.tsx

---
+++ pages/dashboard/Finance.tsx
@@ -0,0 +1,7 @@
+// finance/pages/dashboard/Finance.tsx
+import React from "react";
+import FinancialDashboard from "@/components/Dashboard/FinancialDashboard";
+
+export default function FinancePage() {
+  return <FinancialDashboard />;
+}
