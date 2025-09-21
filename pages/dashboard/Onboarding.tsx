98a039ba3d1d57fff2efb3303bd0b1f091bbf3c9
voycebutler18 <voycebutler1@gmail.com>
2025-09-21 13:17:50 -0500

    Create Onboarding.tsx

---
+++ pages/dashboard/Onboarding.tsx
@@ -0,0 +1,7 @@
+// finance/pages/dashboard/Onboarding.tsx
+import React from "react";
+import OnboardingFlow from "@/components/Dashboard/OnboardingFlow";
+
+export default function OnboardingPage() {
+  return <OnboardingFlow />;
+}
