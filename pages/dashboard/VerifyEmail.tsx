4138ee1930ce87fdcca159501e448e16921c6ab2
voycebutler18 <voycebutler1@gmail.com>
2025-09-21 13:18:12 -0500

    Create VerifyEmail.tsx

---
+++ pages/dashboard/VerifyEmail.tsx
@@ -0,0 +1,7 @@
+// finance/pages/dashboard/VerifyEmail.tsx
+import React from "react";
+import EmailVerificationRequired from "@/components/Auth/EmailVerificationRequired";
+
+export default function VerifyEmailPage() {
+  return <EmailVerificationRequired />;
+}
