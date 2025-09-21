e3b6dd84445a526c97998253b9f99f2a7a24a0a1
voycebutler18 <voycebutler1@gmail.com>
2025-09-21 13:17:28 -0500

    Create Admin.tsx

---
+++ pages/dashboard/Admin.tsx
@@ -0,0 +1,7 @@
+// finance/pages/dashboard/Admin.tsx
+import React from "react";
+import AdminDashboard from "@/components/Dashboard/AdminDashboard";
+
+export default function AdminPage() {
+  return <AdminDashboard />;
+}
